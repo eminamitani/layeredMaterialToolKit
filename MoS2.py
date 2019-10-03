1ad32cd2fe67133df97229de7ac092600234740c
eminamitani <eminamitani@ims.ac.jp>
2019-10-03 14:05:39 +0900

    lattice constant optimization input part

--- MoS2.py
+++ MoS2.py
@@ -2,53 +2,14 @@ from ase.build import mx2
 from ase import Atoms
 from ase.io import read, write
 from ase.calculators.espresso import Espresso
-from mx2 import monolayer_MX2
+from espresso import *
 import os
 
+input_data = "input_data.txt"
+pseudo_dir = "./pseudo"
+config = "config.txt"
+PBS_header="PBS_header.txt"
 
-formula = 'MoS2'
-MoS2 = monolayer_MX2(formula, 3.3, 3.3, 20.0)
-write("test.vasp", MoS2, format='vasp', vasp5=True)
+esp = espresso(input_data=input_data, pseudo_dir=pseudo_dir, config=config, PBS_header=PBS_header)
+esp.optimize_lattice_constant(initial_lattice_constant=3.3,range=5,samples=20,kpts=(12,12,1),vacuum=25.0)
 
-input_data = {
-    'control': {
-        'calculation': 'relax',
-        'outdir': './',
-        'pseudo_dir': './',
-        'prefix': formula,
-        'wf_collect': True
-    },
-    'system': {
-        'ecutwfc': 70,
-        'ecutrho': 560},
-}
-psuedopotentials = {'Mo': 'Mo.upf',
-                    'S': 'S.upf'}
-
-#seems strange, but the psuedopoteitial data is not update
-calc = Espresso(input_data=input_data, psuedopotentials=psuedopotentials, kpts=(12, 12, 1), label=formula)
-
-calc.write_input(MoS2)
-
-with open(formula+".pwi", "r") as input:
-    lines=input.readlines()
-
-datas=[l.rstrip('\n') for l in lines]
-
-for pot in psuedopotentials.keys():
-    print(pot)
-    print(psuedopotentials[pot])
-    for l in range(len(datas)):
-        if (datas[l].find(pot+"_dummy.UPF")) > 0:
-            print('find')
-            ll=datas[l].replace(pot+"_dummy.UPF", psuedopotentials[pot])
-            print(ll)
-            datas[l]=ll
-
-with open(formula+".pwi.rev","w") as input:
-    for l in datas:
-        input.write(l+'\n')
-
-
-#MoS2.set_calculator(calc)
-#MoS2.get_potential_energy()
--- espresso.py
+++ espresso.py
@@ -1,13 +1,131 @@
 from ase.io import read, write
+from ast import literal_eval
+import glob
+import re
+import os
+import shutil
+import numpy as np
+from ase.calculators.espresso import Espresso
+from monolayer import *
+import datetime
 
 class espresso:
 
-    #self.system: ASE Atoms
-    def __init__(self, templete):
-        self.templete=read(templete,format='espresso.in')
+    #input_data: text file include dictionary type data for espresso input
 
-    def optimize_lattice_constant(self):
+    def __init__(self, input_data, config, pseudo_dir, PBS_header):
+
+        with open(input_data, 'r') as inf:
+            self.input_data=literal_eval(inf.read())
+
+        print('espresso base setup')
+        print(self.input_data)
+
+        #constracting psuedopotentials dictionary from psuedo_dir
+        #assume elemental name_option.upf(# UPF)
+        self.pseudopotentials={}
+        files=glob.glob(pseudo_dir+"/*")
+        for f in files:
+            sp=re.split('[._]',os.path.basename(f))
+            self.pseudopotentials[sp[0]]=os.path.basename(f)
+
+        print('avairable pseudopotentials')
+        print(self.pseudopotentials)
+
+        with open(config, 'r') as inf:
+            self.config=literal_eval(inf.read())
+
+        with open(PBS_header, 'r') as header:
+            self.PBS_header=header.read()
+
+        print('PBS header')
+        print(self.PBS_header)
+
+
+    #minor patch program to fix pseudo potential name in pw-input
+    def rename_psuedo(self, input_file):
+
+        with open(input_file, "r") as input:
+            lines = input.readlines()
+
+        datas = [l.rstrip('\n') for l in lines]
+
+        for pot in self.pseudopotentials.keys():
+            for l in range(len(datas)):
+                if (datas[l].find(pot + "_dummy.UPF")) > 0:
+                    #print('find')
+                    ll = datas[l].replace(pot + "_dummy.UPF", self.pseudopotentials[pot])
+                    datas[l] = ll
+
+        shutil.copyfile(input_file, input_file+'.org')
+
+        with open(input_file, "w") as input:
+            for l in datas:
+                input.write(l + '\n')
+
+
+
+    def optimize_lattice_constant(self, initial_lattice_constant, range, samples, kpts, vacuum):
+        '''
+
+        :param initial_lattice_constant: center value to search the lattice constant
+        :param range: percentage to search the lattice constant
+        :param samples: number of samples
+        :param kpts: k-point sampling like (12,12,1)
+        :param vacuum: unit cell size to z direction
+        :return:
+        '''
+
+
+        #in MX2, I set initial guess of thickness of the layer = initial_lattice_constant
+        #this is Ok for typical MX2 like MoS2, WSe2, but be careful
+
+        #add relax tag
+        self.input_data['control']['calculation']='relax'
+
+        calc = Espresso(input_data=self.input_data, psuedopotentials=self.pseudopotentials, kpts=kpts,
+                        label=self.config['formula'])
+
+        #set range to seardh
+        start=initial_lattice_constant-initial_lattice_constant*range/100.0
+        last=initial_lattice_constant+initial_lattice_constant*range/100.0
+
+        latta = np.linspace(start, last, samples)
+        dirs=[]
+        for a in latta:
+            dirname=self.config['formula']+"-a-"+str(a)
+            dirs.append(dirname)
+            os.makedirs(dirname, exist_ok=True)
+            atoms=None
+
+            if(self.config['type']=='MX2'):
+                atoms=monolayer_MX2(self.config['formula'], a, initial_lattice_constant, vacuum)
+
+            calc.write_input(atoms)
+            input_file_name=self.config['formula']+".pwi"
+            self.rename_psuedo(input_file_name)
+            shutil.copyfile(input_file_name , os.path.join(dirname,self.config['formula']+".pwi"))
+
+        input_file_name=self.config['formula']+".pwi"
+        #generate run script
+        with open("run.sh" ,"w") as f:
+            f.write(self.PBS_header)
+            directory=self.config['formula']+"-a-"
+            f.write("for dir in ./"+directory+"*; do \n")
+            f.write("echo $dir \n")
+            f.write("cd $dir \n")
+            f.write(self.config['mpicommand'] +" "+self.config['path']+"pw.x " +self.config['qeoption']+ " -input " + input_file_name + " > scf.out \n")
+            f.write("cd ../ \n")
+            f.write("done \n")
+
+        #create log file
+        logfilename="log_lattice_optim-"+str(datetime.datetime.now())
+        with open(logfilename,"w") as log:
+            for d in dirs:
+                log.write(d+"\n")
+
         return
+
 
 
 
