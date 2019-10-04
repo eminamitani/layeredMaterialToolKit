aa3027acd7b4344e367410c9ed3b0b4b765ca85d
eminamitani <eminamitani@ims.ac.jp>
2019-10-04 14:15:38 +0900

    packaging

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+setup(
+    name='layeredMaterialToolKit',
+    version='0.0.1',
+    paskages=find_packages(),
+    install_requires=[
+        'ase',
+    ],
+)
